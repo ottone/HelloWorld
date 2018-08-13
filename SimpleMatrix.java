fa935d9ceba7bf3b43e5e8710a7786c81ac521ba
Francesco <root@nas.example.com>
2018-08-13 19:16:09 +0200

    Correzioni finali running SimpleMatrix ok

--- SimpleMatrix.java
+++ SimpleMatrix.java
@@ -12,20 +12,29 @@ public class SimpleMatrix
 
 	/* IL CONSTRUCTORS CREA UNA MATRICE 4 X 4 E SETTA I VALORI DI RIGHE E COLONNE */
 
-	public SimpleMatrix ()
+	public SimpleMatrix()
 	{
 		righe = colonne = 4;
 		data = new double[4][4]; /* INIZIALIZZA L'ARRAY DI ARRAY DATA AD UN ARRAY 4 X 4 CON ISTRUZIONE NEW */
 	}
 
+	
+	public SimpleMatrix(int n , int m)
+	{
+		righe = n;
+		colonne = m;
+		data = new double[n][m];
+	}
+
+
 	/* CREA UNA MATRICE DI ZERI @PARAM N NUMERO DI RIGHE @PARAM M NUMERO DI COLONNE */
 	
-	public SimpleMatrix (double[][] initdata)
+	public SimpleMatrix(double[][] initdata)
 	{
 	int i,j;
 	
-		righe = initdata.length /* INIZIALIZZA RIGHE ALLA LUNGHEZZA DELL'ARRAY INITDATA */
-		colonne = initdata[0].length /* STESSA COSA PER LE COLONNE */
+		righe = initdata.length; /* INIZIALIZZA RIGHE ALLA LUNGHEZZA DELL'ARRAY INITDATA */
+		colonne = initdata[0].length; /* STESSA COSA PER LE COLONNE */
 		data = new double[righe][colonne]; /* INIZIALIZZA DATA ALLA DIMENSIONE DELL'ARRAY BIDIMENSIONALE INITDATA CHE VERRA' PASSATO AL METODO SIMPLEMATRIX */
 
 	
@@ -42,14 +51,14 @@ public class SimpleMatrix
 
 	/** SOMMA DI DUE MATRICI @PARAM X IL SECONDO OPERANDO DELLA SOMMA @RETURN UNA MATRICE IL CUI VALORE È THIS+X */
 
-	public SimpleMatrix add (SimpleMatrix x)
+	public SimpleMatrix add(SimpleMatrix x)
 	{
 		SimpleMatrix ris; // OGGETTO SIMPLEMATRIX
 		int i,j;
 
 		if ((righe != x.righe) || (colonne != x.colonne))
-			throw new AritmeticException ("Incompatible Matrix");
-		ris = new SimpleMatrix (righe,colonne);
+			throw new ArithmeticException ("Incompatible Matrix");
+		ris = new SimpleMatrix(righe,colonne);
 		for(i=0 ;i<righe;i++)
 			for(j=0;j<colonne; j++)
 				ris.data[i][j] = data[i][j]+x.data[i][j];
@@ -81,14 +90,14 @@ public class SimpleMatrix
 
 	public SimpleMatrix scalar (double x)
 	{
-		SimpleMatrix x;
+		SimpleMatrix ris;
 		int i, j;
 
-		ris = new SimpleMatrix(righe, colonne);
+		ris =new SimpleMatrix(righe, colonne);
 		
 		for(i=0; i< righe; i++)
 			for(j=0; j <colonne; j++)
-				data[i][j]  *= x;
+				data[i][j]*=x;
 		return ris;
 	}
 		
@@ -121,12 +130,13 @@ public class SimpleMatrix
 	{
 
 		SimpleMatrix mat1, mat2, mat3;
-		double[][] a={{1.2,-0,75,1.8}
-			      {-0.99,3.45,-4.89}
-			      {-9.8,0.08,1.98}};
-		double[][] a1={{1.0,0.0,0.0}
-			       {0.0,1.0,0.0}
-			       {0.0,0.0,1.0}};
+		double[][] a={{1.2 , -0.75 ,1.8},
+			      {-0.99 , 3.45 , -4.89},
+			      {-9.8 , 0.08 , 1.98}};
+
+		double[][] a1={{1.0 , 0.0 , 0.0},
+			       {0.0 , 1.0 , 0.0},
+			       {0.0 , 0.0 , 1.0}};
 		
 		mat1 = new SimpleMatrix(a1);
 		System.out.println (mat1.toString());
